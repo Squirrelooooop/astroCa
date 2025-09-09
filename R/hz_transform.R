28a2fda06df24875ff00172ae42fb0aeaaacfeca
Squirrelooooop <yanlin.s.liu@gmail.com>
2025-09-08 23:09:50 -0700

    fix hz boxcox transformation

--- R/hz_transform.R
+++ R/hz_transform.R
@@ -1,6 +1,7 @@
 #' If Hz histogram is not normal, this use Boxcox transformation
 #' @param hz_model tidy_df
 #' @param df tidy_df
+#' @importFrom fBasics boxcox
 #' @return df with transformed Hz column
 #' @export
 
@@ -13,6 +14,10 @@ hz_transform <- function(df) {
   residuals_p <- hz_model$residuals + c
   tran_lm <- fBasics::boxcox(residuals_p ~ 1, lambda = seq(-2, 2, 0.1), plotit = FALSE)
   lambda <- tran_lm$x[which.max(tran_lm$y)]
-  df$hz_trans <- (df$Hz^lambda - 1) / lambda
+  if (abs(lambda) < 1e-6) {
+    df$hz_trans <- log(df$Hz)
+  } else {
+    df$hz_trans <- (df$Hz^lambda - 1) / lambda
+  }
   return(df)
 }
