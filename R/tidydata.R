7d61b2d770d7844f114b2a0e2b2a157631f68bec
Squirrelooooop <yanlin.s.liu@gmail.com>
2025-09-08 22:14:52 -0700

    Update tidydata.R
    fix bugs and cleanup

--- R/tidydata.R
+++ R/tidydata.R
@@ -1,26 +1,30 @@
 #' Tidy Excel data containing AQUA2 Max Dff
 #' @param df Dataframe needed to be tidied
 #' @param timeinsec Time in seconds for each recording
+#' @importFrom dplyr group_by summarise mutate if_else
+#' @importFrom tidyr pivot_longer drop_na
 #' @return with cols for exp, coverslip, ultrasound, treatment, dFF
 
 
 tidydata <- function(df, timeinsec) {
   df[,5][is.na(df[,5])] <- -9
   tidy_df <- df |>
-    tidyr::pivot_longer(
+    pivot_longer(
                   cols = 5:ncol(df),
                   values_to = "dFF"
-              ) |> tidyr::drop_na() |>
-    tidyr::group_by(exp, coverslip, treatment, ultrasound) |>
-    tidyr::summarise(
+                ) |>
+    drop_na() |>
+    group_by(exp, coverslip, treatment, ultrasound) |>
+    summarise(
       mean_cadff = mean(dFF),
       number_active = n(),
       .groups = "drop"
-    ) |> dplyr::mutate(
+    ) |>
+    mutate(
       Hz = number_active / timeinsec,
-      mean_cadff = dplyr::if_else(mean_cadff == -9, 0, mean_cadff),
-      number_active = dplyr::if_else(mean_cadff == 0, 0L, number_active),
-      Hz = dplyr::if_else(mean_cadff == 0, 0, Hz)
+      mean_cadff = if_else(mean_cadff == -9, 0, mean_cadff),
+      number_active = if_else(mean_cadff == 0, 0L, number_active),
+      Hz = if_else(mean_cadff == 0, 0, Hz)
     )
   tidy_df <- tidy_df[,-5]
   return(tidy_df)
