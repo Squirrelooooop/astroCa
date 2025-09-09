e188449fe873f8fcff38dddd958a8cc0376f6aa4
Squirrelooooop <yanlin.s.liu@gmail.com>
2025-09-08 23:10:54 -0700

    Update hz_transform.R

--- R/hz_transform.R
+++ R/hz_transform.R
@@ -12,7 +12,7 @@ hz_transform <- function(df) {
   hz_model <- lm(Hz ~ treatment * ultrasound, data = df)
   c <- abs(min(hz_model$residuals)) + 1
   residuals_p <- hz_model$residuals + c
-  tran_lm <- fBasics::boxcox(residuals_p ~ 1, lambda = seq(-2, 2, 0.1), plotit = FALSE)
+  tran_lm <- boxcox(residuals_p ~ 1, lambda = seq(-2, 2, 0.1), plotit = FALSE)
   lambda <- tran_lm$x[which.max(tran_lm$y)]
   if (abs(lambda) < 1e-6) {
     df$hz_trans <- log(df$Hz)
