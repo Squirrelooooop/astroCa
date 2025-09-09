ad973b2517bfa407babcfd24b815cbac3319177b
Squirrelooooop <yanlin.s.liu@gmail.com>
2025-09-08 23:03:11 -0700

    Update hz_transform.R
    fix bugs

--- R/hz_transform.R
+++ R/hz_transform.R
@@ -11,7 +11,7 @@ hz_transform <- function(df) {
   hz_model <- lm(Hz ~ treatment * ultrasound, data = df)
   c <- abs(min(hz_model$residuals)) + 1
   residuals_p <- hz_model$residuals + c
-  tran_lm <- fBasic::boxcox(residuals_p ~ 1, lambda = seq(-2, 2, 0.1), plotit = FALSE)
+  tran_lm <- fBasics::boxcox(residuals_p ~ 1, lambda = seq(-2, 2, 0.1), plotit = FALSE)
   lambda <- tran_lm$x[which.max(tran_lm$y)]
   df$hz_trans <- (df$Hz^lambda - 1) / lambda
   return(df)
