1a426e6ad74e194ac507b92dabfb6b2a8c16d41d
Squirrelooooop <yanlin.s.liu@gmail.com>
2025-10-22 15:17:44 -0700

    more flexible hz_transform
    edit hz_transform so it needs both the dataframe and column name to do the boxcox transformation

--- R/hz_transform.R
+++ R/hz_transform.R
@@ -1,6 +1,6 @@
 #' If Hz histogram is not normal, this use Boxcox transformation
-#' @param hz_model tidy_df
 #' @param df tidy_df
+#' @param col_name name of column to transform
 #' @importFrom MASS boxcox
 #' @return df with transformed Hz column
 #' @export
@@ -8,13 +8,13 @@
 
 
 
-hz_transform <- function(df) {
-  tran_lm <- boxcox(df$Hz ~ 1, lambda = seq(-2, 2, 0.1), plotit = FALSE)
+hz_transform <- function(df, col_name) {
+  tran_lm <- boxcox(df$col_name ~ 1, lambda = seq(-2, 2, 0.1), plotit = FALSE)
   lambda <- tran_lm$x[which.max(tran_lm$y)]
   if (abs(lambda) < 1e-6) {
-    df$hz_trans <- log(df$Hz)
+    df$trans <- log(df$col_name)
   } else {
-    df$hz_trans <- (df$Hz^lambda - 1) / lambda
+    df$trans <- (df$col_name^lambda - 1) / lambda
   }
   return(df)
 }
